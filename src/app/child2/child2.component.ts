33e0c65f3c123c8128e52e52ffc0740084d0bea3
xushishishishi <xxushiwen@163.com>
2019-09-27 18:39:40 +0800

    第二个儿子

--- src/app/child2/child2.component.ts
+++ src/app/child2/child2.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-child2',
@@ -12,4 +12,12 @@ export class Child2Component implements OnInit {
   ngOnInit() {
   }
 
+  private _name = '';
+
+  @Input()
+  set name(name: string) {
+    this._name = (name && name.trim()) || '<no name set>';
+  }
+
+  get name(): string { return this._name; }
 }
